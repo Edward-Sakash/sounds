be9f7ae0209f38723d059d6bb813355c91c26760
Edward-Sakash <edward.sakash@yahoo.com>
2023-06-18 19:40:45 +0200

    .

--- bonus.py
+++ bonus.py
@@ -108,7 +108,7 @@ else:
     print("Invalid folder choice.")"""
 #################################################################
 
-import os
+"""import os
 import subprocess
 
 def play_audio(file_path):
@@ -151,3 +151,102 @@ elif folder_choice == 'v':
 
 else:
     print("Invalid folder choice.")
+"""
+###############################################
+
+"""import os
+from playsound import playsound
+import webbrowser
+
+def play_video(file_path):
+    webbrowser.open(file_path)
+
+def play_audio(file_path):
+    playsound(file_path)
+
+# Take user input
+choice = input("If you would like to play 'audio', enter 'a'. Otherwise, enter 'v' to play 'video': ")
+
+if choice.lower() == 'a':
+    # Code to handle audio files
+    folder_choice = input("Choose a folder ('audio_files'): ")
+
+    if folder_choice.lower() == 'audio_files':
+        file_choice = input("Choose a file ('dog'/'cat'): ")
+
+        if file_choice.lower() == 'dog':
+            play_audio(os.path.join("audio_files", "dog_sound.mp3"))
+        elif file_choice.lower() == 'cat':
+            play_audio(os.path.join("audio_files", "cat_sound.mp3"))
+        else:
+            print("Invalid file choice. Please enter 'dog' or 'cat'.")
+    else:
+        print("Invalid folder choice. Please enter 'audio_files'.")
+elif choice.lower() == 'v':
+    # Code to handle video files
+    folder_choice = input("Choose a folder ('video_files'): ")
+
+    if folder_choice.lower() == 'video_files':
+        file_choice = input("Choose a file ('dog'/'cat'): ")
+
+        if file_choice.lower() == 'dog':
+            play_video(os.path.join("video_files", "dog_video.mp4"))
+        elif file_choice.lower() == 'cat':
+            play_video(os.path.join("video_files", "cat_video.mp4"))
+        else:
+            print("Invalid file choice. Please enter 'dog' or 'cat'.")
+    else:
+        print("Invalid folder choice. Please enter 'video_files'.")
+else:
+    print("Invalid input. Please enter 'a' for audio or 'v' for video.")
+"""
+########################################################################
+import os
+from playsound import playsound
+import webbrowser
+
+def play_video(file_path):
+    webbrowser.open(file_path)
+
+def play_audio(file_path):
+    playsound(file_path)
+
+# Take user input
+choice = input("If you would like to play 'audio', enter 'a'. Otherwise, enter 'v' to play 'video': ")
+
+if choice.lower() == 'a':
+    # Code to handle audio files
+    folder_choice = input("Choose a folder ('audio_files'): ")
+
+    if folder_choice.lower() == 'audio_files':
+        file_choice = input("Choose a file ('dog'/'cat'): ")
+
+        if file_choice.lower() == 'dog':
+            play_audio(os.path.join("audio_files", "dog_sound.mp3"))
+        elif file_choice.lower() == 'cat':
+            play_audio(os.path.join("audio_files", "cat_sound.mp3"))
+        else:
+            print("Invalid file choice. Please enter 'dog' or 'cat'.")
+    else:
+        print("Invalid folder choice. Please enter 'audio_files'.")
+elif choice.lower() == 'v':
+    # Code to handle video files
+    folder_choice = input("Choose a folder ('video_files'): ")
+
+    if folder_choice.lower() == 'video_files':
+        file_choice = input("Choose a file ('dog'/'cat'/'panda'/'pandas'): ")
+
+        if file_choice.lower() == 'dog':
+            play_video(os.path.join("video_files", "dog_video.mp4"))
+        elif file_choice.lower() == 'cat':
+            play_video(os.path.join("video_files", "cat_video.mp4"))
+        elif file_choice.lower() == 'panda':
+            play_video(os.path.join("video_files", "panda_video.mp4"))
+        elif file_choice.lower() == 'pandas':
+            play_video(os.path.join("video_files", "panda_video1.mp4"))    
+        else:
+            print("Invalid file choice. Please enter 'dog', 'cat', 'panda', or 'pandas'.")
+    else:
+        print("Invalid folder choice. Please enter 'video_files'.")
+else:
+    print("Invalid input. Please enter 'a' for audio or 'v' for video.")
